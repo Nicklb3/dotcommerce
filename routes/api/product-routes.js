a309ce04f086626aeb85332cfbb159d3596cfc08
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-03-18 00:08:47 -0700

    made a router to get all productData

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  console.log("get Route Start")
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ["name"]
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
+    });
+    console.log(productData)
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product

4dd0bc10770c531ef5ccd48d3e623d796f75bfe5
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-03-18 21:59:32 -0700

    added async to router put

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -61,7 +61,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tagToUpdate = await Tag.update(req.body, {
